3848bbd3c850e81132be812cd6eb986f718ba79b
PriangshuChakraborty <cpriangshu@gmail.com>
2024-03-21 21:55:18 +0530

    added for loop and ifelse

--- compiler.js
+++ compiler.js
@@ -1,12 +1,20 @@
 const code = `
-eta y=6;
+eta y=2
+eta x=0;
+porjonto(x!=y){
+    lekh x;
+    x=x+1;
+}
 lekh y;
 `
 function lexer(input) {
     const tokens = [];
     let cursor = 0;
+    let brackets_loop;
 
     while (cursor < input.length) {
+       
+
         if (/\s/.test(input[cursor])) {
             cursor++;
             continue;
@@ -18,14 +26,18 @@ function lexer(input) {
                 word += input[cursor];
                 cursor++;
             }
-            if (word === 'eta'||word === 'lekh') {
+            if (word === 'porjonto') {
+                    brackets_loop = 0
+                }
+            if (word === 'eta' || word === 'lekh' || word === 'jodi' || word === 'noito' || word === 'porjonto') {
+                
                 tokens.push({ type: 'keyword', value: word });
             } else {
                 tokens.push({ type: 'identifier', value: word });
             }
         }
 
-        if ((/[0-9]/.test(input[cursor]))) {
+        if (/[0-9]/.test(input[cursor])) {
             let number = "";
             while (/[0-9]/.test(input[cursor])||input[cursor] === '.') {
                 number += input[cursor];
@@ -33,8 +45,22 @@ function lexer(input) {
             }
             tokens.push({ type: 'number', value: eval(number) });  
         }
+        
+        if (/[(){}\[\]]/.test(input[cursor])) {
+            if (input[cursor] === '{') {
+                brackets_loop++;
+            } else if (input[cursor] === '}') {
+                brackets_loop--;
+            }
+            if (brackets_loop === 0 && input[cursor] === '}') {
+                tokens.push({ type: 'brackets_loop', value: input[cursor] })
+            } else{
+                tokens.push({ type: 'brackets', value: input[cursor] });
+        }
+             
+        }
 
-        if(/(\+|-|\*|\/|=)/.test(input[cursor])){
+        if(/(\+|-|\*|\/|=|\!)/.test(input[cursor])){
             tokens.push({ type: 'operator', value: input[cursor] });
         }
         if (input[cursor] === '"') {
@@ -69,8 +95,10 @@ function parser(tokens) {
             if (tokens[0].type === 'operator' && tokens[0].value === '=') {
                 tokens.shift();
                 let expression = ''
-                while (tokens[0].type !== 'keyword' && tokens.length > 0) {
-                    if (tokens[0].type !== 'operator' && tokens[1].type === 'identifier') {
+                
+                while (tokens[0]?.type !== 'keyword' && tokens.length > 0) {
+                 
+                    if (tokens[0]?.type !== 'operator' && tokens[1]?.type === 'identifier'&&tokens[0]?.type !== 'brackets') {
                         expression += tokens.shift().value 
                         break
                     }
@@ -87,26 +115,126 @@ function parser(tokens) {
                 name: token.value,
                 value: null
             }
-            if (tokens[0].type === 'operator' && tokens[0].value === '=') {
+            if (tokens[0]?.type === 'operator' && tokens[0].value === '=') {
                 tokens.shift();
                 let expression = ''
-                while (tokens[0].type !== 'keyword' && tokens.length > 0) {
-                    if (tokens[0].type !== 'operator' && tokens[1].type === 'identifier') {
-                        expression += tokens.shift().value 
+                while (tokens[0]?.type !== 'keyword' && tokens.length > 0) {
+                    if (tokens[0]?.type !== 'operator' && tokens[1]?.type === 'identifier'&&tokens[0]?.type !== 'brackets') {
+                        expression += tokens.shift().value
                         break
                     }
                     expression += tokens.shift().value;  
+                    
                 }
                     equation.value= expression.trim();
             }
             ast.body.push(equation);
         }
 
+        if (token.type === 'keyword' && token.value === 'jodi') { 
+            
+            let conditions = {
+                type: 'conditions',
+                value: null
+            }
+            let blockCode = {
+                type: 'blockCode',
+                conditions:null,
+                body: null
+            }
+            let expression = '';
+            if(tokens[0]?.type === 'brackets' && tokens[0].value === '('){
+                tokens.shift();
+                while (tokens[0]?.type !== 'brackets' && tokens.length > 0 && tokens[0]?.value !== ')') {
+                expression += tokens.shift().value;
+            }
+                conditions.value = expression.trim();
+                blockCode.conditions = conditions;
+                tokens.shift();
+            }
+            let blockBody = null;
+            let element = []
+            if (tokens[0]?.type === 'brackets' && tokens[0].value === '{') {
+                tokens.shift();
+                while (tokens[0]?.value !== "}") {
+                    element.push(tokens[0])
+                    tokens.shift()
+                }
+                tokens.shift();
+                blockBody = parser(element);
+                blockCode.body = blockBody;
+                
+            }
+            ast.body.push(blockCode);
+        }
+
+         if (token.type === 'keyword' && token.value === 'noito') { 
+            let blockCode_else  = {
+                type: 'blockCode_else',
+                body: null
+            }
+            let blockBody = null;
+            let element = []
+            if (tokens[0]?.type === 'brackets' && tokens[0].value === '{') {
+                tokens.shift();
+                while (tokens[0].value !== "}") {
+                    element.push(tokens[0])
+                    tokens.shift()
+                }
+                tokens.shift();
+                blockBody = parser(element);
+                blockCode_else.body = blockBody;
+                
+            }
+            ast.body.push(blockCode_else);
+        }
+        if (token.type === 'keyword' && token.value === 'porjonto') { 
+            let conditions = {
+                type: 'conditions',
+                value: null
+            }
+            let blockCode_loop = {
+                type: 'blockCode_loop',
+                conditions:null,
+                body: null
+            }
+            let expression = '';
+            if(tokens[0]?.type === 'brackets' && tokens[0].value === '('){
+                tokens.shift();
+                while (tokens[0]?.type !== 'brackets' && tokens.length > 0 && tokens[0]?.value !== ')') {
+                expression += tokens.shift().value;
+            }
+                conditions.value = expression.trim();
+                blockCode_loop.conditions = conditions;
+                tokens.shift();
+            }
+            let blockBody = null;
+            let element = []
+            if (tokens[0]?.type === 'brackets' && tokens[0].value === '{') {
+                tokens.shift();
+                while (tokens[0].type !== 'brackets_loop') {
+                    element.push(tokens[0])
+                    tokens.shift()
+                  
+                }
+                tokens.shift();
+                blockBody = parser(element);
+                blockCode_loop.body = blockBody;
+                
+            }
+            ast.body.push(blockCode_loop);
+        }
+
         if(token.type === 'keyword' && token.value === 'lekh'){
             let expression = '';
             while (tokens[0]?.type !== 'keyword' && tokens.length > 0) {
+                if (tokens[0]?.type !== 'operator' && tokens[1]?.type === 'identifier'&&tokens[0]?.type !== 'brackets') {
+                        expression += tokens.shift().value + ' ' 
+                        break
+                }
                 expression += tokens.shift().value + ' ';
             }
+            
             ast.body.push({
                 type: 'print',
                 value: expression.trim()
@@ -126,16 +254,19 @@ function codeGenerator(node) {
             return ` ${node.name} = ${node.value};`;
         case 'print':
             return `console.log(${node.value});`;
+        case 'blockCode':
+            return `if(${node.conditions.value}){${node.body.body.map(codeGenerator).join('\n')}}`;
+        case 'blockCode_else':
+            return `else{${node.body.body.map(codeGenerator).join('\n')}}`;
+        case 'blockCode_loop':
+            return `while(${node.conditions.value}){${node.body.body.map(codeGenerator).join('\n')}}`;
     }
 }
 
 function compiler(input) {
     let tokens = lexer(input);
-    console.log(tokens)
     let ast = parser(tokens);
-    console.log(ast)
     let excutableCode = codeGenerator(ast);
-    console.log(excutableCode)
     return excutableCode
 }
 
